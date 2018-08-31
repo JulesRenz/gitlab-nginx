37e86aa9a0a10ca58da5aa1337aacf708fd8d028
Julian Renz <julian@renz.cloud>
2018-08-31 19:36:24 +0200

    add init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo mkdir /srv/gitlab-nginx/gitlab
+sudo mkdir /srv/gitlab-nginx/gitlab/config
+sudo mkdir /srv/gitlab-nginx/gitlab/data
+sudo mkdir /srv/gitlab-nginx/gitlab/logs
+sudo mkdir /srv/gitlab-nginx/nginx/
+sudo mkdir /srv/gitlab-nginx/nginx/certs
+sudo mkdir /srv/gitlab-nginx/nginx/html
+sudo mkdir /srv/gitlab-nginx/nginx/vhost
+sudo mkdir /srv/gitlab-nginx/volumerize/
+sudo mkdir /srv/gitlab-nginx/volumerize/backup
